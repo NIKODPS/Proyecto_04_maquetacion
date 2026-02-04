605050757320895cf9430529cf958a2a428eb707
NIKODPS <nicolas.dps@gmail.com>
2026-02-04 02:10:21 +0100

    2026-02-01

--- App/artForm01.php
+++ App/artForm01.php
@@ -45,19 +45,26 @@ if(!isset($respUser)){
     header('location:/?error=captchaVacio');
     die;  
 }
-
-
 // que la respuesta sea errónea
 if($respUser != $respSystem){
     header('location:/?error=captchaError');
     die; 
 }
 
+
+
 // Recoger el resto de valores del form
 $nombre = $_POST['nombre'];
 $telefono = $_POST['telefono'];
 $email = $_POST['email'];
 $mensaje = $_POST['mensaje'];
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$fecha = date('Y-m-d H:i:s');
+
+// echo $ip . "<br>";
+// echo $fecha;
+// die;
 
 // Zona de comprobaciones DEV
 // echo $nombre.'<br>'.$telefono.'<br>'.$email.'<br>'.$mensaje.'<br>'.$terminos.'<br>'.$respUser.'<br>'.$respSystem;
@@ -125,29 +132,28 @@ $vars = [
     '{asunto}'              => $asunto,
     '{aviso}'               => "Has recibido un correo pidiendo información de $nombre. A continuación sus datos. Ha aceptado los términos de privacidad. ",
     '{explicacion}'         => "Has recibido un correo pidiendo información de $nombre. A continuación sus datos. Ha aceptado los términos de privacidad. ",
-    '{contexto}'                => 'El cliente es ',
+    '{contexto}'            => 'El cliente es ',
     '{razon}'               => 'Si quieres responderle, escríbele al correo que facilita a continuación',
     '{nombre}'              => $nombre,
     '{telefono}'            => $telefono,
     '{email}'               => $email,
-    '{mensaje}'                 => $mensaje,
+    '{mensaje}'             => $mensaje,
     '{responder}'           => 'Procura responder dentro del plazo de 2 días',
+    '{fecha}'               => $fecha,
 
 ];
-
 $cuerpo = str_replace(array_keys($vars), array_values($vars), $html);
-
 include "./envioPhpMailer.php";
 
 
-// 3.1 enviar un correo al ADMIN DE LA WEB
+// 3.2 enviar un correo al USUARIO DE LA WEB
 // recoger más variables que necesita el phpMailer:correo emisor y el nombre emisor,el correo receptor y su nombre, título del correo
 $urlWeb = "http://localhost:3000";
 $correoEmisor =$_ENV['EMAIL_WEB'];
 $nombreEmisor ="Web Panadería";
-$correoDestinatario = $_ENV['EMAIL_ADMIN'];
-$nombreDestinatario= "Admin de la web";
-$asunto = "Has recibido una nueva consulta en la web de $nombre";
+$correoDestinatario = $email;
+$nombreDestinatario= $nombre;
+$asunto = "$nombre, hemos recibido tu consulta.";
 
 // recoger el template con los placeholders
 $html = file_get_contents('./templates/artForm01.html');
@@ -157,24 +163,65 @@ $html = file_get_contents('./templates/artForm01.html');
 $vars = [
     '{url}'                 => $urlWeb,
     '{asunto}'              => $asunto,
-    '{aviso}'               => "Has recibido un correo pidiendo información de $nombre. A continuación sus datos. Ha aceptado los términos de privacidad. ",
-    '{explicacion}'         => "Has recibido un correo pidiendo información de $nombre. A continuación sus datos. Ha aceptado los términos de privacidad. ",
-    '{contexto}'                => 'El cliente es ',
-    '{razon}'               => 'Si quieres responderle, escríbele al correo que facilita a continuación',
+    '{aviso}'               => "$nombre, hemos recibido satisfactóriamente tu consulta.",
+    '{explicacion}'         => "En breve nos pondremos en contacto contigo.",
+    '{contexto}'            => 'Aquí están los datos que nos has facilitado',
+    '{razon}'               => 'Consulta a la Panadería Aginaga',
     '{nombre}'              => $nombre,
     '{telefono}'            => $telefono,
     '{email}'               => $email,
-    '{mensaje}'                 => $mensaje,
-    '{responder}'           => 'Procura responder dentro del plazo de 2 días',
-
+    '{mensaje}'             => $mensaje,
+    '{responder}'           => 'Procuraremos responderte en el plazo de 2 días laborales',
+    '{fecha}'               => $fecha,
 ];
 
 $cuerpo = str_replace(array_keys($vars), array_values($vars), $html);
-
 include "./envioPhpMailer.php";
 
 
 // 4 guardar los datos en una bbdd
+// TODO
+
+// conexión a la DB
+// Configuración de la conexión en $con
+$con = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
+
+// si la conexión es false, sacamos error
+// comprobación de la conexión
+if($con === false){
+    error_log('Error de conexion con la DB: ' . mysqli_connect_error());
+}else{
+    // en caso de que haya conexión, continuamos
+    $con->set_charset("utf8mb4");
+    $sql = "INSERT INTO `consultas`(`nombre`, `telefono`, `email`, `mensaje`, `ip`, `fecha`) VALUES (?,?,?,?,?,?)";
+    $stmt = mysqli_prepare($con, $sql);
+    // ejecutamos el insert del registro en la tabla consultas de la db con un prepare
+    if($stmt === false){
+        error_log('Error al preparar la insercion de la consulta: ' . mysqli_error($con));
+    }else{
+        // inserción definitiva en al DB
+        mysqli_stmt_bind_param($stmt, "ssssss", $nombre, $telefono, $email, $mensaje, $ip, $fecha);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }
+    // salimos
+    mysqli_close($con);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
